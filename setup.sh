6678f8ca8794dede2cbd48cd8e6e2c1142d13181
Ognyan Angelov <oangelov@obecto.com>
2014-07-14 14:58:12 +0300

    Add ack

--- setup.sh
+++ setup.sh
@@ -16,17 +16,27 @@ mkdir -p ~/.vim/colors/tmp; \
 
 curl -o ~/.vim/colors/wombat256.vim 'http://www.vim.org/scripts/download_script.php?src_id=13400'
 
+# Install native dependencies
+# TODO: check package manager
+brew install ack
+
 cd ~/.vim/bundle
+# File Managment
 git clone git@github.com:kien/ctrlp.vim.git
-git clone https://github.com/scrooloose/nerdcommenter.git
+git clone git@github.com:mileszs/ack.vim.git
+git clone git@github.com:scrooloose/nerdtree.git
+
 git clone git@github.com:Lokaltog/powerline.git
+git clone https://github.com/bling/vim-airline
 git clone git@github.com:tomtom/tlib_vim.git
 git clone https://github.com/MarcWeber/vim-addon-mw-utils.git
+git clone git@github.com:tpope/vim-fugitive.git
+
 git clone https://github.com/garbas/vim-snipmate.git
 git clone https://github.com/honza/vim-snippets.git
-git clone https://github.com/bling/vim-airline
+
 git clone git@github.com:Lokaltog/vim-easymotion.git
-git clone git@github.com:tpope/vim-fugitive.git
+git clone https://github.com/scrooloose/nerdcommenter.git
 
 git clone git@github.com:davidhalter/jedi-vim.git
 cd jedi-vim/
@@ -36,7 +46,6 @@ cd ~/.vim/bundle
 mkdir -p ~/.vim/ftplugin
 curl -o ~/.vim/ftplugin/python_editing.vim http://www.vim.org/scripts/download_script.php?src_id=5492
 
-git clone git@github.com:scrooloose/nerdtree.git
 git clone https://github.com/scrooloose/syntastic.git
 git clone git@github.com:Chiel92/vim-autoformat.git
 git clone git@github.com:nvie/vim-flake8.git
