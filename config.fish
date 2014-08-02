385c74ba227284445eedfa06c281e4edd5e2a382
Ognyan Angelov <shambhala@Ognyans-MacBook-Pro.local>
2014-08-02 14:55:49 +0300

    Workon aliases

--- config.fish
+++ config.fish
@@ -17,3 +17,6 @@ set fish_theme robbyrussell
 # setenv JAVA_HOME /Library/Java/JavaVirtualMachines/jdk1.8.0.jdk/Contents/Home
 # set JAVA_HOME /Library/Java/JavaVirtualMachines/jdk1.7.0_60.jdk/Contents/Home/
 
+# Aliases
+alias work_on_blog="source ~/.envs/blog/bin/activate.fish;cd ~/Development/MightyBlog/mightyblog;python manage.py runserver & sleep 1s; open 'http://localhost:8000'"
+alias work_on_checkin="source ~/.envs/checkin/bin/activate.fish;cd cd ~/Development/checkinatfmi_project/checkin-at-fmi/checkinatfmi_project/;python manage.py runserver --settings=checkinatfmi.settings.local"
