36a15c11f5a4d78a57e7e1073a31813ede8ef4fd
Ognyan Angelov <ognyan_angelov@yahoo.com>
2018-02-05 22:59:46 +0200

    Add octave layer and evil move coursor back

--- .spacemacs.d/init.el
+++ .spacemacs.d/init.el
@@ -31,6 +31,7 @@ values."
    ;; List of configuration layers to load.
    dotspacemacs-configuration-layers
    '(
+     octave
      javascript
      markdown
      csv
@@ -349,6 +350,7 @@ you should place your code here."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(evil-move-cursor-back t)
  '(evil-want-Y-yank-to-eol nil)
  '(package-selected-packages
    (quote
